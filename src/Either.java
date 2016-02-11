70e49d96e1836c1b299b7c25b9369a55ee792ebe
gwildr <gwildr@yahoo.se>
2016-02-11 20:56:11 +0100

    Implemented Niklas' Either solution.

--- src/Either.java
+++ src/Either.java
@@ -13,21 +13,58 @@
  signatur (A -> R) -> (B -> R) -> R (fast uttryckt på ett sätt 
  som är möjligt i Java).
 */
-
-public class Either <A,B>{
-	Left<A> left;
-	Right<B> right;
-	
-	public Either(Left<A> left) {
-		this.left = left;
-	}
-
-	public Either (Right<B> right){
-		this.right = right;
-	}
-    public boolean isLeft(Left<A> left){
-    	return true;
+/**
+ * Models the Either type in Haskell
+ * @author gwildr
+ * 
+ * @param <L> the left or wrong type
+ * @param <R> the right or right type
+ */
+public abstract class Either <L,R>{
+    public interface FromEither<L,R,T>{
+    	T fromLeft(L l);
+    	T fromRight(R r);
     }
+    public class Left<L> extends Either<L,R>{
+    	L l;
+    	public Left(L l){
+    		this.l = l;
+    		}
+    	public boolean isLeft(){
+        	return true;
+        }
+    	public boolean isRight(){
+    		return false;
+    	}
+    	
+    	public <T> T either(FromEither<L,R,T> function){
+    		return function.fromLeft(l);
+    	}
+    }
+
+	
+    
+    public class Right<R> extends Either<L,R>{
+    	R r;
+    	public Right(R r){
+    		this.r = r;
+    	}
+    	public boolean isLeft(){
+        	return false;
+        }
+    	public boolean isRight(){
+    		return true;
+    	}
+    	public <T> T either(FromEither<L,R,T> function){
+    		return function.fromRight(r);
+    	}
+    	
+    }
+    
+    public abstract boolean isLeft();
+    public abstract boolean isRight();
+//    public abstract T <T> either();
+    
 	// Return True if the given value is a Left-value, False otherwise.
 	
     // public boolean isRight(){...}
@@ -36,4 +73,5 @@ public class Either <A,B>{
 	
 	
     // public T either((L->T)->(R->T)->T) func){...}
+
 }
